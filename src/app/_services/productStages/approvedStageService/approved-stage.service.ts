6651fe69eb7e63583e1f8bc92af88a3de1eedc1e
aman <amansaini1407@gmail.com>
2025-10-07 23:23:06 +0530

    Product Approval Display

--- src/app/_services/productStages/approvedStageService/approved-stage.service.ts
+++ src/app/_services/productStages/approvedStageService/approved-stage.service.ts
@@ -1,14 +1,20 @@
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { API_AUTHORIZA_URL } from '../../../constants/Constants';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApprovedStageService {
 
+  constructor(private http: HttpClient) {}
 
+  getApprovedProductService(request: any): Observable<any> {
+    return this.http.get(API_AUTHORIZA_URL + 'productApprovedStageController/'
+       + 'productApprovedStage?page=' + request.page + '&size=' + request.size , httpOptions);
+  }
 }
--- src/app/admin-panel/product-stages/product-approved/product-approved.component.ts
+++ src/app/admin-panel/product-stages/product-approved/product-approved.component.ts
@@ -1,17 +1,95 @@
 import { Component, ViewChild } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { NgToastService } from 'ng-angular-popup';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ApprovedStageService } from '../../../_services/productStages/approvedStageService/approved-stage.service';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { UnderReviewStageService } from '../../../_services/productStages/underReviewStageService/under-review-stage.service';
-
 @Component({
   selector: 'app-product-approved',
   templateUrl: './product-approved.component.html',
   styleUrl: './product-approved.component.css',
 })
 export class ProductApprovedComponent {
-  
+  productApprovedList: any[] = [];
+  // productApprovedListClone:any[]=[];
+
+  //Filter List For Searching
+  filteredItems: any = [];
+
+  totalElements: number = 0;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+
+  //SearchList
+  searchText: string = '';
+
+  ngOnInit(): void {
+    this.getApprovedProduct({ page: '0', size: '10' });
+  }
+
+  constructor(
+    private toast: NgToastService,
+    private spinner: NgxSpinnerService,
+    private approvedStageService: ApprovedStageService
+  ) {}
+
+  getApprovedProduct(request: any) {
+    this.spinner.show();
+    this.approvedStageService.getApprovedProductService(request).subscribe({
+      next: (res: any) => {
+        this.productApprovedList = res.data['content'];
+        this.filteredItems = this.productApprovedList;
+
+        this.totalElements = res.data['totalElements'];
+        this.toast.success({detail: 'Success', summary: 'Data Fetch Success',position: 'topRight',duration: 2000,});
+        this.spinner.hide();
+      },
+      error: (err: any) => {
+        console.log(err);
+        this.spinner.hide();
+        this.toast.error({detail: 'Error',summary: err,position: 'bottomRight', duration: 3000, });
+      },
+    });
+  }
+
+  nextPage(event: PageEvent) {
+    console.log(event);
+    const request: any = {};
+    request['page'] = event.pageIndex.toString();
+    request['size'] = event.pageSize.toString();
+    this.getApprovedProduct(request);
+  }
+
+  //Search Starting
+
+  // MULTI SEARCH STARTING
+  onSearch() {
+  const searchQuery = this.searchText.trim().toLowerCase();
+
+  if (searchQuery) {
+    this.filteredItems = this.productApprovedList.filter((item) => {
+      // Convert all values of object into single string
+      const itemData = Object.values(item).join(' ').toLowerCase();
+      return itemData.includes(searchQuery);
+    });
+  } else {
+    this.filteredItems = this.productApprovedList;
+  }
+}
+ // MULTI SEARCH ENDING
+
+
+// SINGLE SEARCH
+  // onSearch() {
+  //   const searchQuery = this.searchText.trim().toLowerCase();
+
+  //   if (searchQuery) {
+  //     this.filteredItems = this.productApprovedList.filter((item) =>
+  //       item.productKey.toLowerCase().includes(searchQuery)
+  //     );
+  //   } else {
+  //     this.filteredItems = this.productApprovedList;
+  //   }
+  // }
+// SINGLE SEARCH
+  //Search Ending
 }
